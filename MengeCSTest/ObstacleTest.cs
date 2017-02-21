83cd92b89d66b6e706c06ca60efced198b6db086
Sean Curtis <Sean Curtis>
2017-02-20 17:17:27 -0800

    Add unit tests to the obstacle functionality
    Tests the winding logic.

---
+++ MengeCSTest/ObstacleTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MengeCS;
+
+namespace MengeCSTest
+{
+    [TestClass]
+    public class ObstacleTest
+    {
+        [TestMethod]
+        public void TestWindingCCWComputation()
+        {
+            Obstacle o = new Obstacle();
+            o.AddPoint(new Vector3(0, 0, 0));
+            o.AddPoint(new Vector3(1, 0, 0));
+            o.AddPoint(new Vector3(1, 0, 1));
+            Assert.IsTrue(o.AgentsWalkOutside());
+            o.ComputeWinding();
+            Assert.IsTrue(o.AgentsWalkOutside());
+        }
+
+        [TestMethod]
+        public void TestWindingCWComputation()
+        {
+            Obstacle o = new Obstacle();
+            o.AddPoint(new Vector3(1, 0, 1));
+            o.AddPoint(new Vector3(1, 0, 0));
+            o.AddPoint(new Vector3(0, 0, 0));
+            Assert.IsTrue(o.AgentsWalkOutside());
+            o.ComputeWinding();
+            Assert.IsFalse(o.AgentsWalkOutside());
+        }
+    }
+}
--- MengeCSTest/Properties/AssemblyInfo.cs
+++ MengeCSTest/Properties/AssemblyInfo.cs
@@ -5,16 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MengeCS")]
+[assembly: AssemblyTitle("MengeCSTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MengeCS")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("MengeCSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: InternalsVisibleTo("MengeCSTest")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12227357-1ddd-4b19-8eda-d3d1b6066904")]
+[assembly: Guid("3ce51881-1245-4cf6-b66c-fba7f5dd2cbb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
